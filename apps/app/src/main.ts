02ec3de393bc11505e8ad85fb79da299e6db4c72
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-27 17:13:36 +0530

    create the microservice for product

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
+  //create the microservice for product
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
+      //set the kafka to connect the product for gateway
       transport: Transport.KAFKA,
       options: {
         client: {
