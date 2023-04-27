12b7a3ed8d7c8688b274d913a139a170863f7e78
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-27 18:12:22 +0530

    create the auth controllers

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -10,26 +10,31 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { RolesGuard } from '../product/guards/role/roles.guard';
 import { Roles } from '../product/roles.decorator';
 
+//set the controller for auth
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  //create the user register
   @Post('/signup')
   signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signUp(signUpDto);
   }
 
-  @Get('/login')
+  //create the user login
+  @Post('/login')
   login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
     return this.authService.login(loginDto);
   }
 
+  //get the all users
   @Get('/all')
   @UseGuards(UserRoleGuard)
   getAll(): Promise<User[]> {
     return this.authService.getAllUsers();
   }
 
+  //create the user by admin
   @Post('/create-user')
   @UseGuards(RolesGuard)
   @Roles('admin')
