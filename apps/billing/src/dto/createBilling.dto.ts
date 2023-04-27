665bd854e99c4841f34026dd38f9a15d009808ca
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-27 19:00:23 +0530

    fix bugs

--- apps/billing/src/dto/createBilling.dto.ts
+++ apps/billing/src/dto/createBilling.dto.ts
@@ -1,34 +1,18 @@
-// // import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
-//import { Types } from 'mongoose';
+// import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+// import { Types } from 'mongoose';
 export class CreateBillingDto {
-//    readonly shippingInfo: {
-//       readonly address: string;
-//       readonly city: string;
-//       readonly state: string;
-//       readonly country: string;
-//       readonly pinCode: number;
-//       readonly phoneNo: number;
-//     };
-//     readonly orderItems: {
-//       readonly productName: string;
-//       readonly productPrice: number;
-//       readonly quantity: number;
-//       readonly productImage: string;
-//       readonly productId: Types.ObjectId | string;
-//     }[];
-//     readonly user: Types.ObjectId | string;
-//     readonly paymentInfo: {
-//       readonly id: string;
-//       readonly status: string;
-//     };
-//     readonly paidAt: Date;
-//     readonly itemsPrice: number;
-//     readonly taxPrice: number;
-//     readonly shippingPrice: number;
-//     readonly totalPrice: number;
-//     readonly orderStatus: string;
-//     readonly deliveredAt?: Date;
-//     readonly createdAt: Date;
+   readonly paymentInfo: {
+      readonly id: string;
+      readonly status: string;
+    };
+     readonly paidAt: Date;
+     readonly itemsPrice: number;
+     readonly taxPrice: number;
+     readonly shippingPrice: number;
+     readonly totalPrice: number;
+     readonly orderStatus: string;
+     readonly deliveredAt?: Date;
+     readonly createdAt: Date;
   }
   
 
--- apps/billing/src/interface/billing.interface.ts
+++ apps/billing/src/interface/billing.interface.ts
@@ -1,33 +1,15 @@
-import { IProduct } from 'apps/app/src/interface/product.interface';
-//mport mongoose, { Document, Types } from 'mongoose';
- export interface IBilling extends Document{
-//     readonly shippingInfo: {
-//        readonly address: string;
-//        readonly city: string;
-//        readonly state: string;
-//        readonly country: string;
-//        readonly pinCode: number;
-//        readonly phoneNo: number;
-//       };
-//      readonly orderItems: {
-//        readonly productName: string;
-//        readonly productPrice: number;
-//        readonly quantity: number;
-//        readonly productImage: string;
-//        readonly productId: Types.ObjectId | IProduct;
-//       }[];
-//      readonly user: Types.ObjectId;
-//      readonly paymentInfo: {
-//        readonly id: string;
-//        readonly status: string;
-//       };
-//      readonly paidAt: Date;
-//      readonly itemsPrice: number;
-//      readonly taxPrice: number;
-//      readonly shippingPrice: number;
-//      readonly totalPrice: number;
-//      readonly orderStatus: string;
-//      readonly deliveredAt?: Date;
-//      readonly createdAt: Date;
+export interface IBilling extends Document{
+   readonly paymentInfo: {
+       readonly id: string;
+       readonly status: string;
+       };
+     readonly paidAt: Date;
+     readonly itemsPrice: number;
+     readonly taxPrice: number;
+     readonly shippingPrice: number;
+     readonly totalPrice: number;
+     readonly orderStatus: string;
+     readonly deliveredAt?: Date;
+     readonly createdAt: Date;
      }
     
